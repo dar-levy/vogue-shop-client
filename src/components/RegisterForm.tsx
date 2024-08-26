aa829c9f7235b79479d4433c41f145972dd2fd4f
Dar Levy <darlevy8@gmail.com>
2024-08-27 01:49:06 +0300

    Prettify register form

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,11 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 
+import React from "react";
 import Joi from "joi-browser";
 import * as userService from "../services/userService";
-import auth from "../services/authService";
 import { toast } from "react-toastify";
 import Form from "./common/Form.tsx";
+import { TextField, Button, Container, Typography, Box } from "@mui/material";
 
 class RegisterForm extends Form {
   state = {
@@ -33,17 +34,55 @@ class RegisterForm extends Form {
     }
   };
 
+  renderTextField(name, label, type = "text") {
+    const { data, errors } = this.state;
+
+    return (
+        <TextField
+            fullWidth
+            variant="outlined"
+            margin="normal"
+            name={name}
+            label={label}
+            type={type}
+            value={data[name]}
+            onChange={this.handleChange}
+            error={!!errors[name]}
+            helperText={errors[name]}
+        />
+    );
+  }
+
   render() {
     return (
-      <div>
-        <h1>Register</h1>
-        <form onSubmit={this.handleSubmit}>
-          {this.renderInput("username", "Username")}
-          {this.renderInput("password", "Password", "password")}
-          {this.renderInput("name", "Name")}
-          {this.renderButton("Register")}
-        </form>
-      </div>
+        <Container component="main" maxWidth="xs">
+          <Box
+              sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+          >
+            <Typography component="h1" variant="h5">
+              Register
+            </Typography>
+            <Box component="form" onSubmit={this.handleSubmit} sx={{ mt: 1 }}>
+              {this.renderTextField("username", "Username")}
+              {this.renderTextField("password", "Password", "password")}
+              {this.renderTextField("name", "Name")}
+              <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  sx={{ mt: 3, mb: 2 }}
+              >
+                Register
+              </Button>
+            </Box>
+          </Box>
+        </Container>
     );
   }
 }
