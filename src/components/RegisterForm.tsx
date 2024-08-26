999345d3341f598589ceaef85b99693e0b4e949e
Dar Levy <darlevy8@gmail.com>
2024-08-27 01:30:29 +0300

    Create register form

---
+++ src/components/RegisterForm.tsx
@@ -0,0 +1,51 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
+import Joi from "joi-browser";
+import * as userService from "../services/userService";
+import auth from "../services/authService";
+import { toast } from "react-toastify";
+import Form from "./common/Form.tsx";
+
+class RegisterForm extends Form {
+  state = {
+    data: { username: "", password: "", name: "" },
+    errors: {},
+  };
+
+  schema = {
+    username: Joi.string().required().email().label("Username"),
+    password: Joi.string().required().min(5).label("Password"),
+    name: Joi.string().required().label("Name"),
+  };
+
+  doSubmit = async () => {
+    try {
+      await userService.register(this.state.data);
+      window.location = "/";
+      toast.success("Successfully Registered");
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Register</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderInput("name", "Name")}
+          {this.renderButton("Register")}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default RegisterForm;
