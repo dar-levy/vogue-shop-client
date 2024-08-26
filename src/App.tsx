b894e1d7ff1edb7f42f34b5f086d0cbfcd9d6615
Dar Levy <darlevy8@gmail.com>
2024-08-26 17:08:16 +0300

    Create header for catalog

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
 import './App.css'
+import {Container, createTheme, ThemeProvider} from "@mui/material";
+import Catalog from "./components/catalog/Catalog.tsx";
+import {useState} from "react";
+import Header from "./components/Header.tsx";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const palleteType = darkMode ? 'dark' : 'light';
+  const theme = createTheme({
+    palette: {
+      mode: palleteType,
+      background: {
+        default: (palleteType === 'light') ? '#eaeaea' : '#121212'
+      }
+    }
+  })
+
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
 
   return (
-      <h1>Hello world!</h1>
+      <ThemeProvider theme={theme}>
+        <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+        <Container>
+          <Catalog />
+        </Container>
+      </ThemeProvider>
   )
 }
 
