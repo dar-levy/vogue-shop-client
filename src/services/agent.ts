c9bdf07a89f07ea4f2ff0da42f5b64ef3bffe437
Dar Levy <darlevy8@gmail.com>
2024-08-27 13:50:55 +0300

    Remove redundant test errors tests

--- src/services/agent.ts
+++ src/services/agent.ts
@@ -51,14 +51,6 @@ const Catalog = {
     details: (id: number) => requests.get(`products/${id}`)
 }
 
-const TestErrors = {
-    get400Error: () => requests.get('buggy/bad-request'),
-    get401Error: () => requests.get('buggy/unauthorised'),
-    get404Error: () => requests.get('buggy/not-found'),
-    get500Error: () => requests.get('buggy/server-error'),
-    getValidationError: () => requests.get('buggy/validation-error')
-}
-
 const Basket = {
     get: () => requests.get('basket'),
     addItem: (productId: number, quantity = 1) => requests.post(`basket?productId=${productId}&quantity=${quantity}`, {}),
@@ -67,7 +59,6 @@ const Basket = {
 
 const agent = {
     Catalog,
-    TestErrors,
     Basket
 }
 
