302588de9698a0e6a3de3548805ef69b4900e430
Dar Levy <darlevy8@gmail.com>
2024-08-28 16:43:36 +0300

    Update Form

--- src/components/catalog/NewProduct.tsx
+++ src/components/catalog/NewProduct.tsx
@@ -1,19 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 
-import React from "react";
 import Joi from "joi-browser";
 import { toast } from "react-toastify";
-import { TextField, Button, Grid, Typography, Paper, Avatar } from "@mui/material";
-import { v4 as uuidv4 } from 'uuid';
 import Form from "../common/Form.tsx";
-// import { saveProduct } from "../../services/productService";
-
 
 class NewProduct extends Form {
     state = {
         data: {
-            id: uuidv4(),
             name: "",
             description: "",
             price: "",
@@ -36,8 +30,8 @@ class NewProduct extends Form {
 
     doSubmit = async () => {
         try {
-            // await saveProduct(this.state.data);
-            this.props.navigate("/catalog");
+            await saveProduct(this.state.data);
+            window.location("/catalog");
             toast.success("Saved successfully.");
         } catch (err) {
             toast.error("Could not save the product");
@@ -48,82 +42,25 @@ class NewProduct extends Form {
         const { pictureUrl } = this.state.data;
 
         return (
-            <Paper elevation={3} sx={{ padding: 3, maxWidth: 600, margin: "auto" }}>
-                <Typography variant="h4" component="h1" gutterBottom>
-                    Product Form
-                </Typography>
+            <div className="profile-form-container">
+                <h1>Product Form</h1>
                 {pictureUrl && (
-                    <Grid container justifyContent="center" sx={{ marginBottom: 2 }}>
-                        <Avatar
-                            alt="Product Image"
-                            src={pictureUrl}
-                            sx={{ width: 100, height: 100 }}
-                        />
-                    </Grid>
+                    <div className="avatar-container">
+                        <img src={pictureUrl} alt="Picture URL" className="avatar" />
+                    </div>
                 )}
                 <form onSubmit={this.handleSubmit}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            {this.renderTextField("name", "Name")}
-                        </Grid>
-                        <Grid item xs={12}>
-                            {this.renderTextField("description", "Description")}
-                        </Grid>
-                        <Grid item xs={12}>
-                            {this.renderTextField("price", "Price")}
-                        </Grid>
-                        <Grid item xs={12}>
-                            {this.renderTextField("brand", "Brand")}
-                        </Grid>
-                        <Grid item xs={12}>
-                            {this.renderTextField("type", "Type")}
-                        </Grid>
-                        <Grid item xs={12}>
-                            {this.renderTextField("pictureUrl", "Picture URL")}
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                color="primary"
-                                disabled={this.isFormInvalid()}
-                            >
-                                Save
-                            </Button>
-                        </Grid>
-                    </Grid>
+                    {this.renderInput("name", "Name")}
+                    {this.renderInput("description", "Description")}
+                    {this.renderInput("price", "Price")}
+                    {this.renderInput("brand", "Brand")}
+                    {this.renderInput("type", "Type")}
+                    {this.renderInput("pictureUrl", "Picture URL")}
+                    {this.renderButton("Save")}
                 </form>
-            </Paper>
+            </div>
         );
     }
-
-    renderTextField(name, label) {
-        const { data, errors } = this.state;
-        return (
-            <TextField
-                fullWidth
-                variant="outlined"
-                label={label}
-                name={name}
-                value={data[name]}
-                onChange={this.handleChange}
-                error={!!errors[name]}
-                helperText={errors[name]}
-            />
-        );
-    }
-
-    isFormInvalid = () => {
-        const { data, errors } = this.state;
-
-        // Check if there are any errors or if any input field is empty
-        const hasErrors = Object.keys(errors).length > 0;
-        const hasEmptyFields = Object.values(data).some(value => value.trim() === "");
-
-        return hasErrors || hasEmptyFields;
-    };
-
 }
 
 export default NewProduct;
