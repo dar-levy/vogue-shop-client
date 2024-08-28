e0c2c2be2e825f5d9744d7796ecc1824c3b5680b
Dar Levy <darlevy8@gmail.com>
2024-08-28 17:31:15 +0300

    Import saveProduct from service

--- src/components/catalog/NewProduct.tsx
+++ src/components/catalog/NewProduct.tsx
@@ -8,6 +8,7 @@ import {Avatar, Box, Container, Typography} from "@mui/material";
 import React from "react";
 import * as config from "../../config.json"
 import { v4 as uuidv4 } from 'uuid';
+import { saveProduct } from "../../services/productService.ts";
 
 
 class NewProduct extends Form {
