efb9f727b3041733326f27230789976e98417a48
Dar Levy <darlevy8@gmail.com>
2024-08-28 20:33:06 +0300

    Create Search input in catalog

--- src/components/catalog/ProductsGrid.tsx
+++ src/components/catalog/ProductsGrid.tsx
@@ -1,5 +1,6 @@
-import { Grid } from "@mui/material";
-import {Product} from "../../models/product.ts";
+import { useState } from "react";
+import { Grid, TextField } from "@mui/material";
+import { Product } from "../../models/product.ts";
 import ProductCard from "./ProductCard.tsx";
 
 interface Props {
@@ -7,13 +8,32 @@ interface Props {
 }
 
 export default function ProductsGrid({ products }: Props) {
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const filteredProducts = products.filter((product) =>
+        product.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        product.brand.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        product.type?.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
     return (
-        <Grid container spacing={4}>
-            {products.map(product => (
-                <Grid item xs={3} key={product.id}>
-                    <ProductCard product={product} />
-                </Grid>
-            ))}
-        </Grid>
-    )
+        <>
+            <TextField
+                label="Search Products"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                sx={{ position: "sticky", top: 0, zIndex: 10, marginBottom: 5, marginTop: 0 }}
+            />
+            <Grid container spacing={4}>
+                {filteredProducts.map(product => (
+                    <Grid item xs={12} sm={6} md={4} lg={3} key={product.id}>
+                        <ProductCard product={product} />
+                    </Grid>
+                ))}
+            </Grid>
+        </>
+    );
 }
