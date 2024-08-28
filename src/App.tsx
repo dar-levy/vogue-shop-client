5e897f0360e2934e30490e5d33a3fffe157ab3ea
Dar Levy <darlevy8@gmail.com>
2024-08-28 03:09:44 +0300

    Extract all routes handling from App to Routes.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
 import './App.css';
 import {Container, createTheme, CssBaseline, ThemeProvider} from "@mui/material";
-import Catalog from "./components/catalog/Catalog.tsx";
 import { useEffect, useState } from "react";
 import Header from "./components/Header.tsx";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import NotFound from './components/NotFound.tsx';
-import RegisterForm from "./components/RegisterForm.tsx";
-import LogoutForm from "./components/LogoutForm.tsx";
-import LoginForm from './components/LoginForm.tsx';
-import About from './components/About.tsx';
-import Contact from './components/Contact.tsx';
-import Basket from './components/basket/Basket.tsx';
-import ProductDetails from "./components/catalog/ProductDetails.tsx";
 import agent from "./services/agent.ts";
 import {useStoreContext} from "./context/StoreContext.tsx";
 import Loading from './components/Loading.tsx';
-import Checkout from "./components/Checkout.tsx";
-import ThankYou from "./components/ThankYou.tsx";
-import Reviews from "./components/Reviews.tsx";
-import {NewArrivals} from "./components/NewArrivals.tsx";
-import ActivityHistory from "./components/ActivityHistory.tsx";
+import AppRoutes from "./router/Routes.tsx";
 
 function App() {
   const { setBasket, isAdmin, isAuthenticated } = useStoreContext();
@@ -73,33 +59,7 @@ function App() {
           <CssBaseline />
           <Header darkMode={darkMode} handleThemeChange={handleThemeChange} isAuthenticated={isAuthenticated} />
           <Container>
-            <Routes>
-              {isAuthenticated ? (
-                  <>
-                    <Route path="/catalog" element={<Catalog />} />
-                    <Route path="/catalog/:id" element={<ProductDetails />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route path="/reviews" element={<Reviews />} />
-                    <Route path="/new-arrivals" element={<NewArrivals />} />
-                    {isAdmin && <Route path="/activity-history" element={<ActivityHistory/>}/>}
-                    <Route path="/basket" element={<Basket />} />
-                    <Route path="/checkout" element={<Checkout />} />
-                    <Route path="/thank-you/:id" element={<ThankYou />} />
-                    <Route path="/logout" element={<LogoutForm />} />
-                    <Route path="/not-found" element={<NotFound />} />
-                    <Route path="/" element={<Navigate to="/catalog" />} />
-                    <Route path="*" element={<Navigate to="/not-found" />} />
-                  </>
-              ) : (
-                  <>
-                    <Route path="/register" element={<RegisterForm />} />
-                    <Route path="/login" element={<LoginForm />} />
-                    <Route path="/" element={<Navigate to="/login" />} />
-                  </>
-              )}
-              <Route path="*" element={<Navigate to={isAuthenticated ? "/catalog" : "/login"} />} />
-            </Routes>
+            <AppRoutes />
           </Container>
         </ThemeProvider>
       </>
---
+++ src/router/Routes.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import {useStoreContext} from "../context/StoreContext.tsx";
+import Catalog from "../components/catalog/Catalog.tsx";
+import ProductDetails from "../components/catalog/ProductDetails.tsx";
+import About from '../components/About.tsx';
+import Contact from '../components/Contact.tsx';
+import Reviews from '../components/Reviews.tsx';
+import { NewArrivals } from '../components/NewArrivals.tsx';
+import ActivityHistory from '../components/ActivityHistory.tsx';
+import Basket from '../components/basket/Basket.tsx';
+import Checkout from '../components/Checkout.tsx';
+import ThankYou from '../components/ThankYou.tsx';
+import LogoutForm from '../components/LogoutForm.tsx';
+import NotFound from '../components/NotFound.tsx';
+import RegisterForm from '../components/RegisterForm.tsx';
+import LoginForm from '../components/LoginForm.tsx';
+
+const AppRoutes: React.FC = () => {
+    const { isAdmin, isAuthenticated } = useStoreContext();
+
+    return (
+        <Routes>
+            {isAuthenticated ? (
+                <>
+                    <Route path="/catalog" element={<Catalog />} />
+                    <Route path="/catalog/:id" element={<ProductDetails />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/reviews" element={<Reviews />} />
+                    <Route path="/new-arrivals" element={<NewArrivals />} />
+                    {isAdmin && <Route path="/activity-history" element={<ActivityHistory />} />}
+                    <Route path="/basket" element={<Basket />} />
+                    <Route path="/checkout" element={<Checkout />} />
+                    <Route path="/thank-you/:id" element={<ThankYou />} />
+                    <Route path="/logout" element={<LogoutForm />} />
+                    <Route path="/not-found" element={<NotFound />} />
+                    <Route path="/" element={<Navigate to="/catalog" />} />
+                    <Route path="*" element={<Navigate to="/not-found" />} />
+                </>
+            ) : (
+                <>
+                    <Route path="/register" element={<RegisterForm />} />
+                    <Route path="/login" element={<LoginForm />} />
+                    <Route path="/" element={<Navigate to="/login" />} />
+                </>
+            )}
+            <Route path="*" element={<Navigate to={isAuthenticated ? "/catalog" : "/login"} />} />
+        </Routes>
+    );
+};
+
+export default AppRoutes;
