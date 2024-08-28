e823a2fa843328a3218e242275bbbf5ff6a7f77b
Dar Levy <darlevy8@gmail.com>
2024-08-28 14:20:53 +0300

    Set commented code for fetching products

--- src/components/catalog/Catalog.tsx
+++ src/components/catalog/Catalog.tsx
@@ -1,23 +1,28 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 import {useState, useEffect} from 'react';
 import ProductsGrid from "./ProductsGrid.tsx";
 import Loading from '../Loading.tsx';
 import agent from "../../services/agent.ts";
 import {useStoreContext} from "../../context/StoreContext.tsx";
+import {getProducts} from "../../services/productService.ts";
+import {toast} from "react-toastify";
 
 
 export default function Catalog() {
     const [loading, setLoading] = useState(true);
     const { products, setProducts } = useStoreContext();
 
-    useEffect(() => {
+    useEffect(async () => {
         setProducts(agent.Catalog.list())
         setLoading(false)
-        // agent.Catalog.list()
-        //     .then(products => {
-        //         setProducts(products)
-        //     })
-        //     .catch(error => console.log(error))
-        //     .finally(() => setLoading(false));
+        // try {
+        //     const { data } = await getProducts();
+        //     setProducts(data)
+        // } catch (err) {
+        //     toast.error("Could not get the products");
+        // }
     }, [])
 
 
